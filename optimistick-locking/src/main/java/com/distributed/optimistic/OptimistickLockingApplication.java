e1bd257c5fe123482a653b222d96d209fc3870fb
srinivassankoji <srinivaschary.chary@gmail.com>
2026-01-09 16:12:42 +0530

    Optimistic Locking

---
+++ optimistick-locking/src/main/java/com/distributed/optimistic/OptimistickLockingApplication.java
@@ -0,0 +1,13 @@
+package com.distributed.optimistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimistickLockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimistickLockingApplication.class, args);
+	}
+
+}
